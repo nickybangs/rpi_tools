56978fc6f2817266e1e0471c57332a9056efc583
Nicky Bangs <nicky.bangs@gmail.com>
2022-08-28 18:12:53 -0400

    add client side ftp tools for rpi

---
+++ rpi_tools/ftp_cli.py
@@ -0,0 +1,35 @@
+import atexit
+from ftplib import FTP
+import os
+
+import rpi_tools.ftp_config as ftp_config
+
+
+class FTP_IF:
+    def __init__(self, config=None):
+        if config is None:
+            self.config = ftp_config.default
+        else:
+            self.config = config
+        ftp_server = self.config['server']
+        user = self.config['user']
+        passwd = self.config['passwd']
+
+        self.ftp = FTP(ftp_server)
+        self.ftp.login(user, passwd)
+        atexit.register(self.ftp.quit)
+
+    def get(self, localf, remotef):
+        self.ftp.retrbinary("RETR " + remotef ,open(localf, 'wb').write)
+
+    def put_stream(self, stream, remotef):
+        self.ftp.storlines(f"STOR {remotef}", stream)
+
+    def put(self, localf, remotef):
+        self.ftp.storbinary(f"STOR {remotef}", open(localf, "rb"), 1024)
+
+if __name__ == "__main__":
+    ftp_cli = FTP_IF()
+    local_fname = "/tmp/note_kjhlzl.txt"
+    remote_fname = "/files/test.txt"
+    ftp_cli.put(local_fname, remote_fname)
